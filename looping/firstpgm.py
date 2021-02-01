6345a96ba22b187419bbf4ca41d3c7f22857c80b
sruthi Sasidharan <sruthisasidharan15@gmail.com>
2021-02-01 11:34:43 +0530

    looping

---
+++ looping/firstpgm.py
@@ -0,0 +1,5 @@
+#initialisation
+i=10
+while(i>=1):
+    print(i)
+    i-=1
---
+++ looping/sumofnum.py
@@ -0,0 +1,7 @@
+num=int(input("enter limit"))
+i=1
+total=0
+while(i<=num):
+    total=total+i#0+1=1+2=3
+    i+=1#2 3
+print(total)#1 3
